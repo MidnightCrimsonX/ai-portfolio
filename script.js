b4dad79369caf7841d61589d0df558acd9048c5b
MidnightCrimsonX <daviz.ivander21@gmail.com>
2026-06-02 13:24:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const TM_MODEL_URL = "./my-model/";
 // API key Gemini dari aistudio.google.com
 // INGAT: key ini terlihat publik di GitHub
-const GEMINI_KEY = "AQ.Ab8RN6KBBMxnZK9tdmf1tjk8PjlHZp7t_5B2Wg1Aq8XSvze4qg"; // ganti dengan key kamu
+const GEMINI_KEY = "AQ.Ab8RN6KxTWrnrOlKwOzN669jYBKTkybuVj24I6saFBDfUMClaw" // ganti dengan key kamu
 // Nama model Gemini yang digunakan
 const GEMINI_MODEL = "gemini-2.5-flash";
 // STATE — variabel yang dibutuhkan oleh beberapa fungsi
